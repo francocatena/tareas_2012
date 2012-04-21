3ae0e980ed6caa882588281757b2a7841effd76e
Franco Catena <francocatena@gmail.com>
2012-04-21 12:49:08 -0300

    Clase 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tareas::Application.config.secret_token = '6ef20f6ea485ea7585f371b5bdb7a8414ce3ab13425a7f5578a1b2bfd430fc45a9569abdd9a7a888894c01535a7c644377f77f17ae7c56415e5e29f575de9a6d'
