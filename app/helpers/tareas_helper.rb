cc33fe1a5726bb868ef10c12733879741b919835
Franco Catena <francocatena@gmail.com>
2012-06-15 15:33:11 -0300

    Mejoras en vistas con Twitter Bootstrap

--- app/helpers/tareas_helper.rb
+++ app/helpers/tareas_helper.rb
@@ -1,6 +1,9 @@
 module TareasHelper
   def show_tarea_responsable_select(form)
-    form.select :responsable_id, Responsable.all.map { |r| [r.to_s, r.id] },
+    form.input(
+      :responsable_id,
+      collection: Responsable.all.map { |r| [r.to_s, r.id] },
       include_blank: true
+    )
   end
 end
